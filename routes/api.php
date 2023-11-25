401e42c760af07f6ff6abaf581ece4a9bddbd000
Muhammad Imam <saitamasensei2005@gmail.com>
2023-11-25 20:06:38 +0700

    pepek

---
+++ routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+
+    Route::get('/profile', function(Request $request) {
+        return auth()->user();
+    });
+
+    Route::resource('/user', UserController::class);
+    
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
